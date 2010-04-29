3e41c155dd25f981df4b0332e1bbf378970c6ef9
Javier Jardón <jjardon@gnome.org>
2010-04-29 17:42:51 +0200

    Update autotools configuration a bit
    New requirements:
    autoconf >= 2.63
    libtool >= 2.2.6

--- autogen.sh
+++ autogen.sh
@@ -13,9 +13,9 @@ test ${TEST_TYPE} ${FILE} || {
 }
 
 which gnome-autogen.sh || {
-        echo "*** You need to install gnome-common from GNOME SVN:"
+        echo "*** You need to install gnome-common from GNOME Git:"
         echo "***  git clone git://git.gnome.org/gnome-common"
         exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.11 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
